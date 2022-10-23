1c6f1f6dc19a0ddc2bea93fca80560241f36bf75
Sergey Litvinov <slitvinov@gmail.com>
2022-10-22 22:16:02 -0400

    minor

--- WaveEquation.cpp
+++ WaveEquation.cpp
@@ -8,12 +8,12 @@
 
 using namespace std;
 void Update(std::complex<double> *x, int lengthofx, int k, int m,
-            std::complex<double> *h, std::complex<double> *s,
-            std::vector<std::complex<double> *> v);
+	    std::complex<double> *h, std::complex<double> *s,
+	    std::vector<std::complex<double> *> v);
 void GeneratePlaneRotation(std::complex<double> &dx, std::complex<double> &dy,
-                           std::complex<double> &cs, std::complex<double> &sn);
+			   std::complex<double> &cs, std::complex<double> &sn);
 void ApplyPlaneRotation(std::complex<double> &dx, std::complex<double> &dy,
-                        std::complex<double> &cs, std::complex<double> &sn);
+			std::complex<double> &cs, std::complex<double> &sn);
 
 enum {N = 32, L = 64}; /* N is the number of spatial steps, L is the
 			  number of time steps */
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
   // Right hand side vector
   std::complex<double> b[N * L];
   std::complex<double> massContig[3 * N - 2];
-  
+
   // Temp vectors used in GMRES rotuine
   std::complex<double> temp[N * L];
   std::complex<double> temp2[N * L];
@@ -46,7 +46,7 @@ int main(int argc, char **argv) {
   int totalnodes, mynode;
   std::vector<std::complex<double> *> Wblocks, Ablocks;
   std::complex<double> **UMonolithic, **UtMonolithic;
-  
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &totalnodes);
   MPI_Comm_rank(MPI_COMM_WORLD, &mynode);
@@ -107,23 +107,23 @@ int main(int argc, char **argv) {
       CreateTridiag(N, temp);
       SetTriDiagEqualTo(N, mass, temp);
       MultiplyTriDiagByConst(
-          N, (1.0 - 2.0 * D[i * L + i] + D[i * L + i] * D[i * L + i]), temp);
+	  N, (1.0 - 2.0 * D[i * L + i] + D[i * L + i] * D[i * L + i]), temp);
       AddTriDiag(N, temp, stiffA0, tridaigVec[i]);
     }
     // Reserve memory for contiguous counterparts
     for (int i = 0; i < L; i++) {
       std::complex<double> *pointertolargeblocked =
-          new std::complex<double>[3 * N - 2];
+	  new std::complex<double>[3 * N - 2];
       Wblocks.push_back(pointertolargeblocked);
     }
     // Fill the contiguous counterparts
     for (int i = 0; i < L; i++) {
       for (j = 0; j < N - 1; j++)
-        Wblocks[i][j] = std::get<0>(tridaigVec[i])[j];
+	Wblocks[i][j] = std::get<0>(tridaigVec[i])[j];
       for (j = 0; j < N; j++)
-        Wblocks[i][N - 1 + j] = std::get<1>(tridaigVec[i])[j];
+	Wblocks[i][N - 1 + j] = std::get<1>(tridaigVec[i])[j];
       for (j = 0; j < N - 1; j++)
-        Wblocks[i][2 * N - 1 + j] = std::get<2>(tridaigVec[i])[j];
+	Wblocks[i][2 * N - 1 + j] = std::get<2>(tridaigVec[i])[j];
       Wblocks[i][N - 1] = 1;
       Wblocks[i][2 * N - 2] = 1;
     }
@@ -160,17 +160,17 @@ int main(int argc, char **argv) {
     // Reserve memory for contiguous counterparts
     for (int i = 0; i < L; i++) {
       std::complex<double> *pointertolargeblocked =
-          new std::complex<double>[3 * N - 2];
+	  new std::complex<double>[3 * N - 2];
       Ablocks.push_back(pointertolargeblocked);
     }
     // Fill the contiguous counterparts
     for (int i = 0; i < L; i++) {
       for (j = 0; j < N - 1; j++)
-        Ablocks[i][j] = std::get<0>(tridaigVec[i])[j];
+	Ablocks[i][j] = std::get<0>(tridaigVec[i])[j];
       for (j = 0; j < N; j++)
-        Ablocks[i][N - 1 + j] = std::get<1>(tridaigVec[i])[j];
+	Ablocks[i][N - 1 + j] = std::get<1>(tridaigVec[i])[j];
       for (j = 0; j < N - 1; j++)
-        Ablocks[i][2 * N - 1 + j] = std::get<2>(tridaigVec[i])[j];
+	Ablocks[i][2 * N - 1 + j] = std::get<2>(tridaigVec[i])[j];
       Ablocks[i][N - 1] = 1;
       Ablocks[i][2 * N - 2] = 1;
     }
@@ -183,8 +183,8 @@ int main(int argc, char **argv) {
 
     for (i = 0; i < L; i++) {
       for (j = 0; j < L; j++) {
-        UMonolithic[i][j] = F[j * L + i];
-        UtMonolithic[i][j] = Ft[j * L + i];
+	UMonolithic[i][j] = F[j * L + i];
+	UtMonolithic[i][j] = Ft[j * L + i];
       }
     }
 
@@ -207,24 +207,24 @@ int main(int argc, char **argv) {
       u0[i] = sin(2 * M_PI * i * h);
 
     /*
-            // The non-smooth initial condition.
-            for(i=0;i<N;i++)
-            {
-               if(i*h<0.5-1.0/8 || i*h > 0.5+1.0/8)
-                   u0[i] = 0.0;
-               else
-               {
-                   u0[i] = cos(4*M_PI*(i*h-0.5))*cos(4*M_PI*(i*h-0.5));
-               }
-            }
+	    // The non-smooth initial condition.
+	    for(i=0;i<N;i++)
+	    {
+	       if(i*h<0.5-1.0/8 || i*h > 0.5+1.0/8)
+		   u0[i] = 0.0;
+	       else
+	       {
+		   u0[i] = cos(4*M_PI*(i*h-0.5))*cos(4*M_PI*(i*h-0.5));
+	       }
+	    }
     */
     U0[0] = massContig[N - 1] * u0[0] + massContig[2 * N - 1] * u0[1];
     for (j = 1; j < N - 1; j++) {
       U0[j] = massContig[j - 1] * u0[j - 1] + massContig[N - 1 + j] * u0[j] +
-              massContig[2 * N - 1 + j] * u0[j + 1];
+	      massContig[2 * N - 1 + j] * u0[j + 1];
     }
     U0[N - 1] =
-        massContig[2 * N - 2] * u0[N - 1] + massContig[N - 2] * u0[N - 2];
+	massContig[2 * N - 2] * u0[N - 1] + massContig[N - 2] * u0[N - 2];
 
     for (i = 0; i < N; i++)
       b[i] = U0[i];
@@ -281,13 +281,13 @@ int main(int argc, char **argv) {
 
   // Calculation of ||P^{-1}b||
   ApplyPreconditioner(mynode, totalnodes, N, L, UMonolithic, UtMonolithic,
-                      Wblocks, b, temp);
+		      Wblocks, b, temp);
   CalculateNorm(mynode, totalnodes, N, L, temp, normb);
   // Calculation of r = P^{-1}(b-A*x)
   MultiplyByWaveSystem(mynode, totalnodes, N, L, Ablocks, massContig, x, temp);
   VectorSubtraction(mynode, totalnodes, N, L, b, temp, temp2);
   ApplyPreconditioner(mynode, totalnodes, N, L, UMonolithic, UtMonolithic,
-                      Wblocks, temp2, r0);
+		      Wblocks, temp2, r0);
 
   // Calculate beta = ||r||
   CalculateNorm(mynode, totalnodes, N, L, r0, beta);
@@ -314,28 +314,28 @@ int main(int argc, char **argv) {
     for (i = 0; i < m && j <= max_iter; i++, j++) {
 
       if (complete)
-        continue;
+	continue;
 
       //==========================================  ARNODLI BEGIN
       MultiplyByWaveSystem(mynode, totalnodes, N, L, Ablocks, massContig, v[i],
-                           temp);
+			   temp);
       ApplyPreconditioner(mynode, totalnodes, N, L, UMonolithic, UtMonolithic,
-                          Wblocks, temp, temp2);
+			  Wblocks, temp, temp2);
       for (k = 0; k <= i; k++) {
-        // w = temp2
-        std::complex<double> dotprodoutput;
-        DotProduct(mynode, totalnodes, N, L, temp2, v[k], dotprodoutput);
-        H[k + i * m] = dotprodoutput;
-        PlusEqualTo(mynode, totalnodes, N, L, v[k], temp2,
-                    -1.0 * dotprodoutput);
+	// w = temp2
+	std::complex<double> dotprodoutput;
+	DotProduct(mynode, totalnodes, N, L, temp2, v[k], dotprodoutput);
+	H[k + i * m] = dotprodoutput;
+	PlusEqualTo(mynode, totalnodes, N, L, v[k], temp2,
+		    -1.0 * dotprodoutput);
       }
       CalculateNorm(mynode, totalnodes, N, L, temp2, H[(i + 1) + i * m]);
       SetEqualTo(mynode, totalnodes, N, L, temp2, v[i + 1],
-                 (1.0 / H[(i + 1) + i * m]));
+		 (1.0 / H[(i + 1) + i * m]));
       //=========================================  ARNOLDI END
 
       for (k = 0; k < i; k++)
-        ApplyPlaneRotation(H[k + i * m], H[(k + 1) + i * m], cs[k], sn[k]);
+	ApplyPlaneRotation(H[k + i * m], H[(k + 1) + i * m], cs[k], sn[k]);
 
       GeneratePlaneRotation(H[i + i * m], H[(i + 1) + i * m], cs[i], sn[i]);
       ApplyPlaneRotation(H[i + i * m], H[(i + 1) + i * m], cs[i], sn[i]);
@@ -343,10 +343,10 @@ int main(int argc, char **argv) {
 
       resid = std::abs(s[i + 1]);
       if (resid.real() / normb.real() < tol.real()) {
-        Update(x, N * L, i, m, H, s, v);
-        tol = resid;
-        max_iter = j;
-        complete = 1;
+	Update(x, N * L, i, m, H, s, v);
+	tol = resid;
+	max_iter = j;
+	complete = 1;
       }
     }
     if (complete)
@@ -354,10 +354,10 @@ int main(int argc, char **argv) {
     Update(x, N * L, m - 1, m, H, s, v);
     // Calculation of r = P^{-1}(b-A*x)
     MultiplyByWaveSystem(mynode, totalnodes, N, L, Ablocks, massContig, x,
-                         temp);
+			 temp);
     VectorSubtraction(mynode, totalnodes, N, L, b, temp, temp2);
     ApplyPreconditioner(mynode, totalnodes, N, L, UMonolithic, UtMonolithic,
-                        Wblocks, temp2, r0);
+			Wblocks, temp2, r0);
 
     // Calculate beta = ||r||
     CalculateNorm(mynode, totalnodes, N, L, r0, beta);
@@ -377,8 +377,8 @@ int main(int argc, char **argv) {
 // Definitions of the routines used in GMRES
 
 void Update(std::complex<double> *x, int lengthofx, int k, int m,
-            std::complex<double> *h, std::complex<double> *s,
-            std::vector<std::complex<double> *> v) {
+	    std::complex<double> *h, std::complex<double> *s,
+	    std::vector<std::complex<double> *> v) {
   std::complex<double> *y = new std::complex<double>[k + 1];
   for (int i = 0; i < k + 1; i++)
     y[i] = s[i];
@@ -399,7 +399,7 @@ void Update(std::complex<double> *x, int lengthofx, int k, int m,
 }
 
 void GeneratePlaneRotation(std::complex<double> &dx, std::complex<double> &dy,
-                           std::complex<double> &cs, std::complex<double> &sn) {
+			   std::complex<double> &cs, std::complex<double> &sn) {
   if (dy == 0.0) {
     cs = 1.0;
     sn = 0.0;
@@ -415,7 +415,7 @@ void GeneratePlaneRotation(std::complex<double> &dx, std::complex<double> &dy,
 }
 
 void ApplyPlaneRotation(std::complex<double> &dx, std::complex<double> &dy,
-                        std::complex<double> &cs, std::complex<double> &sn) {
+			std::complex<double> &cs, std::complex<double> &sn) {
   std::complex<double> temp = cs * dx + sn * dy;
   dy = -sn * dx + cs * dy;
   dx = temp;
